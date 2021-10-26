dd6cddd6da81da009f8c98e39f7ff4250f96d78b
Cameron Helkuik <cameronhelkuik@gmail.com>
2021-10-25 20:17:47 -0500

    Added score to the UI

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -22,9 +22,12 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
+        FindObjectOfType<GameStatus>().AddToScore();
+
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
 
         level.BlockDestroyed();
     }
+
 }
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
+    //configuration params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
-    // Start is called before the first frame update
-    void Start()
+    //state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Start()
     {
-        
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
