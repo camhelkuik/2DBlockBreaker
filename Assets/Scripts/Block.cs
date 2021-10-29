bf306489abc839761a791b168b23452d9dcadf1c
Cameron Helkuik <cameronhelkuik@gmail.com>
2021-10-28 20:54:38 -0500

    added different breakable levels of blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,24 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    //config params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparlesVFX;
+    [SerializeField] int MaxHits;
+    [SerializeField] Sprite[] hitSprites;
 
     //cached reference
     Level level;
 
+    //state variables
+    [SerializeField] int timesHit; //TODO only serialized for debug purposes
+
     private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
     {
         level = FindObjectOfType<Level>();
 
-        level.CountBreakableBlocks();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+
+        if (timesHit >= MaxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
