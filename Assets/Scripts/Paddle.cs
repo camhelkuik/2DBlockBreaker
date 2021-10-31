e31959696767b1e618b0e7df031ddc401982bff9
Cameron Helkuik <cameronhelkuik@gmail.com>
2021-10-31 16:42:13 -0500

    added auto play

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -8,24 +8,38 @@ public class Paddle : MonoBehaviour
     [SerializeField] float minX = 1f;
     [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
-    
+
+    //cached references
+    GameSession theGameSession;
+    Ball theBall;
 
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-
         //creating paddle position
         Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
 
         //updating just the paddle x position based on mouse
-        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
+        paddlePosition.x = Mathf.Clamp(GetXPos(), minX, maxX);
         
         transform.position = paddlePosition;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
